22217bc72bfc53e4e800b4793f9f8bf5093fa0a7
abhilash abhi <gujjetiabhilash1289@gmail.com>
2021-11-29 09:43:53 +0530

    css and javascript basis

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+/*writing content using alert */
+alert("Look at me, i'm writing my 1st javascript!!!")
+for(var i=1;i<=10;i++){
+  document.write(i*i + "<br/>");
+  
+}
+
+ //concatenation
+ console.log("my name is "+ name + "and i am aged"+ abhi);
+ //template string
+ console.log('my name is ${name} and i am aged ${x}');
---
+++ javascript/traversy.js
@@ -0,0 +1,42 @@
+console.log('hello world');
+console.error('abhi');
+console.warn('warning abhi')
+document.write("Hello World <br/>");
+alert("heyyy hii");
+// var(globally scoped) let and const
+var x=35.9 ;
+const a=20;
+
+console.log(a);
+
+let b=40;
+b=20;
+console.log(b);
+
+
+var c=a+b;
+var k ="abhilash.GUJJETI";
+var abhi=true;
+let s="abhi, btech, jntuhcej, 2018";
+let p=["abhi"," btech"," jntuhcej"," 2018"];
+
+document.write(k.length +"<br/>");
+document.write(k.toLowerCase() +"<br/>");
+document.write(k.toUpperCase() +"<br/>");
+document.write(k.substring(0,4) +"<br/>");
+document.write(s.split(",")+ "<br/>");
+document.write(k.substring(0,4).toUpperCase() +"<br/>");
+document.write(a+"<br/>")
+document.write(b+"<br/>")
+document.write(p[1]);
+console.log("abhi")
+//concatenation
+
+console.log("my name is "+ x +"and i am aged"+ abhi );
+
+//template string
+
+console.log(`my name is ${k} and i am aged ${a}`);
+
+
+
